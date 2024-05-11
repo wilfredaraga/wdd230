b774955ae3b554b80655669104e12bd52f873529
wilfredaraga <wilfredeiron@gmail.com>
2024-05-11 22:06:33 +0800

    updated

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,19 @@ document.querySelector('#year').innerHTML = d.getFullYear();
 
 let dateModified = new Date(document.lastModified);
 document.querySelector('#lastModified').innerHTML = `Last Modification: ${dateModified}`;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+})
+
+
+const modeButton = document.querySelector('#mode');
+const main = document.querySelector('main');
+
+modeButton.addEventListener('click', () => {
+    main.classList.toggle('darkMode')
+})
