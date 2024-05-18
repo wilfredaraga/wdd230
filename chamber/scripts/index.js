2523c49ac95007596b461fe007680632d5ea97dd
wilfredaraga <wilfredeiron@gmail.com>
2024-05-18 23:19:00 +0800

    W02 Project Milestone: The Chamber Home Page

---
+++ chamber/scripts/index.js
@@ -0,0 +1,17 @@
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+const searchBox = document.querySelector('.searchBox');
+const contactUs = document.querySelector('.contactUs')
+
+hambutton.addEventListener('click', () => {
+    hambutton.classList.toggle('show');
+    mainnav.classList.toggle('show');
+    searchBox.classList.toggle('show');
+    contactUs.classList.toggle('show');
+})
+
+const d = new Date()
+document.querySelector('#year').innerHTML = d.getFullYear();
+
+let dateModified = new Date(document.lastModified);
+document.querySelector('#lastModified').innerHTML = `Last Modification: ${dateModified}`;
